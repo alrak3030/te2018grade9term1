8fe130f4789e3d5923b773a1ee5f4307a6ba53a4
karla <karlaj@csedge.org>
2018-10-06 13:19:29 -0700

    added to data.js

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 // Example
-let message = "Hello World!";
+let message = "boo World!";
 console.log(message);
-
+let bDayMonth = 7
+let animal = "horse"
+let bDayYear = 3
+let age = 15
+let name = "karla"
+console.log(name)
+console.log(age)
+console.log(bDayYear)
+console.log(animal)
+console.log(bDayMonth)
 // Type your code below!
